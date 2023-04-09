bb4a2ba97c6d4868c21a41c774c323e4c8688032
Zeradeil <wandark@ya.ru>
2023-04-09 14:23:09 +0700

    решено 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,4 +4,51 @@
 // [345, 897, 568, 234] -> 2
 
 
+int NumMassive = ReadLine("Введите размер массива => ");
+int[] massive = FillArray();
+PrintArray(massive);
+CountNumbers(massive);
 
+int ReadLine(string massage)
+{
+    System.Console.Write(massage);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int[] FillArray()
+{
+    int[] array = new int[NumMassive];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] FillArray)
+{
+    for (int i = 0; i < massive.Length; i++)
+    {
+        System.Console.Write($"{massive[i]} ");
+    }
+}
+
+void CountNumbers(int[] massive)
+{
+    int count = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        System.Console.WriteLine("В массиве нет четных чисел");
+    }
+    else
+    {
+        System.Console.WriteLine($"-> {count}");
+    }
+}
