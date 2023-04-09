8294fbfb6d1bb80000ac93d839f3f74b450ac039
Zeradeil <wandark@ya.ru>
2023-04-09 15:49:21 +0700

    решено 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,6 +3,51 @@
 
 // [3 7 22 2 78] -> 76
 
+int NumArray = ReadLine("Введите размер массива => ");
+int[] Array = FillArray();
+PrintArray(Array);
+DifMaxMin(Array);
 
+void DifMaxMin(int[] Array)
+{
+    int min = Array[0];
+    int max = Array[0];
+    for (int i = 1; i < Array.Length; i++)
+    {
+        if (max < Array[i])
+        {
+            max = Array[i];
+        }
+        if (min > Array[i])
+        {
+            min = Array[i];
+        }
+    }
+    System.Console.WriteLine($"=> {max-min}");
+}
+
+int ReadLine(string massage)
+{
+    System.Console.Write(massage);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int[] FillArray()
+{
+    int[] array = new int[NumArray];
+    for (int i = 0; i < NumArray; i++)
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+void PrintArray(int[] Array)
+{
+    for (int i = 0; i < NumArray; i++)
+    {
+        System.Console.Write($"{Array[i]} ");
+    }
+}
 
 
